5a21e030fb8f32342c90a8e2766d18a757f5e74c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-20 10:23:48 +0100

    AddedM lib/three_d_modeling.dart M  test/widget_test.dart

---
+++ lib/three_d_modeling.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThreeDModeling extends StatefulWidget {
+  const ThreeDModeling({super.key});
+
+  @override
+  State<ThreeDModeling> createState() => _ThreeDModelingState();
+}
+
+class _ThreeDModelingState extends State<ThreeDModeling> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: ,);
+  }
+}
