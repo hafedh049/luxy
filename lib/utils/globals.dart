9678f26bddc7c893074e7be2c18e07a0c35c3e55
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-23 16:45:20 +0100

    ModifiedM lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color transparent = Colors.transparent;
-const Color pink = Color.fromARGB(255, , g, b);
+const Color pink = Color.fromARGB(255, 255, 77, 103);
+const Color dark = Color.fromARGB(255, 23, 26, 33);
