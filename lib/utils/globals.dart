0e9af6b9821cc86a2411ae6061247e5141bca98a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-23 16:51:47 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -8,3 +8,4 @@ const Color white = Color.fromARGB(255, 255, 255, 255);
 const Color whiteGrey = Color.fromARGB(255, 243, 243, 234);
 const Color darkGrey = Color.fromARGB(255, 57, 57, 57);
 const Color black = Color.fromARGB(255, 15, 15, 15);
+const Color darkKeyboard = Color.fromARGB(255, 31, 34, 43);
