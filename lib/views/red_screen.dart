0dc60a76a168775bfe88e38fdc4936990c63fbbe
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 17:11:31 +0100

    AddedM lib/views/red_screen.dart

---
+++ lib/views/red_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RedScreenOfDeath extends StatelessWidget {
+  const RedScreenOfDeath({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[],
+      ),
+    );
+  }
+}
