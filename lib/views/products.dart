09fa80e45fe6c65aa0057b4bc00037de240c87ea
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 22:28:59 +0100

    ModifiedM lib/views/products.dart

--- lib/views/products.dart
+++ lib/views/products.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:luxy/models/product_model.dart';
@@ -18,7 +17,7 @@ class Products extends StatefulWidget {
 
 class _ProductsState extends State<Products> {
   final TextEditingController _searchController = TextEditingController();
-
+  final GlobalKey<State> _filterKey = GlobalKey<State>();
   final List<String> _labels = <String>["Products"];
 
   @override
@@ -36,6 +35,7 @@ class _ProductsState extends State<Products> {
         Container(
           decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), border: Border.all(width: .5, color: white)),
           child: TextField(
+            onChanged: (String value) => _filterKey.currentState!.setState(() {}),
             controller: _searchController,
             decoration: InputDecoration(
               border: InputBorder.none,
@@ -62,65 +62,70 @@ class _ProductsState extends State<Products> {
         const Text("FEATURED PRODUCTS", style: TextStyle(color: white, fontSize: 18, fontWeight: FontWeight.bold)),
         const SizedBox(height: 20),
         Expanded(
-          child: FutureBuilder<List<ProductModel>>(
-            future: loadProducts(),
-            builder: (BuildContext context, AsyncSnapshot<List<ProductModel>> snapshot) {
-              if (snapshot.hasData) {
-                if (snapshot.data!.isNotEmpty) {
-                  final List<ProductModel> products = snapshot.data!;
-                  return GridView.builder(
-                    shrinkWrap: true,
-                    padding: EdgeInsets.zero,
-                    itemCount: products.length,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: .85),
-                    itemBuilder: (BuildContext context, int index) => Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Container(
-                          height: 150,
-                          width: 150,
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            boxShadow: <BoxShadow>[BoxShadow(color: pink.withOpacity(.3), blurStyle: BlurStyle.outer, offset: const Offset(2, 2))],
-                            image: const DecorationImage(image: AssetImage("assets/pictures/me.jpeg"), fit: BoxFit.cover),
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              if (Random().nextBool())
-                                Chip(
-                                  backgroundColor: pink,
-                                  elevation: 8,
-                                  padding: const EdgeInsets.all(4),
-                                  shadowColor: pink.withOpacity(.3),
-                                  avatar: const Icon(FontAwesome.crown_solid, size: 15, color: Colors.yellow),
-                                  label: const Text(" MOST SELLED", style: TextStyle(color: white, fontSize: 14, fontWeight: FontWeight.w500)),
-                                ),
-                              const Spacer(),
-                              Chip(
-                                backgroundColor: pink,
-                                elevation: 8,
-                                padding: const EdgeInsets.all(4),
-                                shadowColor: pink.withOpacity(.3),
-                                label: Text(products[index].productPrice, style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+          child: StatefulBuilder(
+            key: _filterKey,
+            builder: (BuildContext context, void Function(void Function()) _) {
+              return FutureBuilder<List<ProductModel>>(
+                future: loadProducts(),
+                builder: (BuildContext context, AsyncSnapshot<List<ProductModel>> snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data!.isNotEmpty) {
+                      final List<ProductModel> products = snapshot.data!;
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        padding: EdgeInsets.zero,
+                        itemCount: products.length,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: .85),
+                        itemBuilder: (BuildContext context, int index) => Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              height: 150,
+                              width: 150,
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                boxShadow: <BoxShadow>[BoxShadow(color: pink.withOpacity(.3), blurStyle: BlurStyle.outer, offset: const Offset(2, 2))],
+                                image: const DecorationImage(image: AssetImage("assets/pictures/me.jpeg"), fit: BoxFit.cover),
                               ),
-                            ],
-                          ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  if (Random().nextBool())
+                                    Chip(
+                                      backgroundColor: pink,
+                                      elevation: 8,
+                                      padding: const EdgeInsets.all(4),
+                                      shadowColor: pink.withOpacity(.3),
+                                      avatar: const Icon(FontAwesome.crown_solid, size: 15, color: Colors.yellow),
+                                      label: const Text(" MOST SELLED", style: TextStyle(color: white, fontSize: 14, fontWeight: FontWeight.w500)),
+                                    ),
+                                  const Spacer(),
+                                  Chip(
+                                    backgroundColor: pink,
+                                    elevation: 8,
+                                    padding: const EdgeInsets.all(4),
+                                    shadowColor: pink.withOpacity(.3),
+                                    label: Text(products[index].productPrice, style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Flexible(child: Text(products[index].productName, style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)))
+                          ],
                         ),
-                        const SizedBox(height: 10),
-                        Flexible(child: Text(products[index].productName, style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)))
-                      ],
-                    ),
-                  );
-                } else {
-                  return const Center(child: Text("NO PRODUCTS YET", style: TextStyle(color: white, fontSize: 25, fontWeight: FontWeight.w500, letterSpacing: 2)));
-                }
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const LoadingScreen();
-              }
-              return RedScreenOfDeath(error: snapshot.error.toString());
+                      );
+                    } else {
+                      return const Center(child: Text("NO PRODUCTS YET", style: TextStyle(color: white, fontSize: 25, fontWeight: FontWeight.w500, letterSpacing: 2)));
+                    }
+                  } else if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const LoadingScreen();
+                  }
+                  return RedScreenOfDeath(error: snapshot.error.toString());
+                },
+              );
             },
           ),
         ),
