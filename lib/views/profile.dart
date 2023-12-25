ac6480b1222be5d0c8f46501a80ebaf2865e0f47
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 16:52:06 +0100

    ModifiedM lib/views/profile.dart

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -12,188 +12,184 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: dark,
-        centerTitle: true,
-        title: const Text("Profile", style: TextStyle(color: white, fontSize: 18, letterSpacing: 2, fontWeight: FontWeight.w500)),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  CircleAvatar(radius: 15, backgroundImage: AssetImage("assets/pictures/me.png")),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text("Hafedh Guenichi", style: TextStyle(color: white, fontSize: 18, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                      Text("Following 23", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            const Text("Settings", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.user, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Edit Profile", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.bell, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Notification", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(Bootstrap.translate, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Language", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Text("English (US)", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  SizedBox(width: 20),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Row(
-                children: <Widget>[
-                  const Icon(FontAwesome.eye, size: 15, color: white),
-                  const SizedBox(width: 20),
-                  const Text("Dark Mode", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  const Spacer(),
-                  StatefulBuilder(
-                    builder: (BuildContext context, void Function(void Function()) setS) {
-                      return Switch(
-                        value: user.get("dark_theme"),
-                        onChanged: (bool value) => user.put("dark_theme", !user.get("dark_theme")),
-                        activeColor: pink,
-                        inactiveThumbColor: grey,
-                        inactiveTrackColor: white,
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.shield_solid, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Security", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            const Text("Orders", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.cart_shopping_solid, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Manage my orders", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.bell, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Order history", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.location_arrow_solid, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Manage shipping address", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            const Text("Services", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.lock_solid, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Privacy policy", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.info_solid, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Help Center", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Row(
-                children: <Widget>[
-                  Icon(FontAwesome.users_solid, size: 15, color: white),
-                  SizedBox(width: 20),
-                  Text("Invite friends", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
-                  Spacer(),
-                  Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
-                ],
-              ),
-            ),
+            Text("Profile", style: TextStyle(color: white, fontSize: 18, letterSpacing: 2, fontWeight: FontWeight.w500)),
           ],
         ),
-      ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              CircleAvatar(radius: 15, backgroundImage: AssetImage("assets/pictures/me.png")),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text("Hafedh Guenichi", style: TextStyle(color: white, fontSize: 18, letterSpacing: 2, fontWeight: FontWeight.w500)),
+                  Text("Following 23", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const Text("Settings", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+        const SizedBox(height: 10),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.user, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Edit Profile", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.bell, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Notification", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(Bootstrap.translate, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Language", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Text("English (US)", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              SizedBox(width: 20),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Row(
+            children: <Widget>[
+              const Icon(FontAwesome.eye, size: 15, color: white),
+              const SizedBox(width: 20),
+              const Text("Dark Mode", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              const Spacer(),
+              StatefulBuilder(
+                builder: (BuildContext context, void Function(void Function()) setS) {
+                  return Switch(
+                    value: user.get("dark_theme"),
+                    onChanged: (bool value) => user.put("dark_theme", !user.get("dark_theme")),
+                    activeColor: pink,
+                    inactiveThumbColor: grey,
+                    inactiveTrackColor: white,
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.shield_solid, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Security", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        const Text("Orders", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+        const SizedBox(height: 10),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.cart_shopping_solid, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Manage my orders", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.bell, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Order history", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.location_arrow_solid, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Manage shipping address", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        const Text("Services", style: TextStyle(color: grey, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+        const SizedBox(height: 10),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.lock_solid, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Privacy policy", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.info_solid, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Help Center", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Row(
+            children: <Widget>[
+              Icon(FontAwesome.users_solid, size: 15, color: white),
+              SizedBox(width: 20),
+              Text("Invite friends", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500)),
+              Spacer(),
+              Icon(FontAwesome.chevron_right_solid, size: 15, color: white),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
