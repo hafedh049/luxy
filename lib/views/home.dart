b111df951a2db31854e784204c4a02321da44065
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 16:58:07 +0100

    ModifiedM lib/views/home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -30,16 +30,16 @@ class _HomeState extends State<Home> {
           rippleColor: grey,
           haptic: true,
           tabBorderRadius: 15,
-          tabActiveBorder: Border.all(color: grey, width: 1), // tab button border
-          tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
-          tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
-          curve: Curves.easeOutExpo, // tab animation curves
-          duration: 500.ms, // tab animation duration
-          gap: 8, // the tab button gap between icon and text
-          color: Colors.grey[800], // unselected icon color
-          activeColor: Colors.purple, // selected icon and text color
+          tabActiveBorder: Border.all(color: grey, width: 1),
+          tabBorder: Border.all(color: grey, width: 1),
+          tabShadow: [BoxShadow(color: grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
+          curve: Curves.easeOutExpo,
+          duration: 500.ms,
+          gap: 8,
+          color: grey, // unselected icon color
+          activeColor: pink, // selected icon and text color
           iconSize: 24, // tab button icon size
-          tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
+          tabBackgroundColor: pink.withOpacity(.1), // selected tab background color
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
           tabs: [
             GButton(
