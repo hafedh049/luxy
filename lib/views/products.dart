6e955f4fa15eadcf3148283277e16908ca0b50f2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 20:00:40 +0100

    ModifiedM lib/views/products.dart

--- lib/views/products.dart
+++ lib/views/products.dart
@@ -11,7 +11,15 @@ class _ProductsState extends State<Products> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[],
+      children: <Widget>[
+        const SizedBox(height: 30),
+        TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
     );
   }
 }
