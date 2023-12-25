ea936aceb915720177b987152037f1766ef7db12
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 17:15:31 +0100

    ModifiedM lib/views/red_screen.dart

--- lib/views/red_screen.dart
+++ lib/views/red_screen.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:luxy/utils/globals.dart';
 
 class RedScreenOfDeath extends StatelessWidget {
-  const RedScreenOfDeath({super.key});
-
+  const RedScreenOfDeath({super.key, required this.error});
+  final String error;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
+            SizedBox(width: MediaQuery.sizeOf(context).width * .6, child: Lottie.asset("assets/lotties/error.json")),
             const SizedBox(height: 20),
-            Flexible(child: Text("Privacy policy", style: TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500))),
+            Flexible(child: Text(error, style: const TextStyle(color: white, fontSize: 16, letterSpacing: 2, fontWeight: FontWeight.w500))),
           ],
         ),
       ),
