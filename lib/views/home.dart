1c496ed078127f72c5d123d709cc7f498814dab2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 17:00:07 +0100

    ModifiedM lib/views/home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:luxy/utils/globals.dart';
 import 'package:luxy/views/profile.dart';
 
@@ -27,38 +28,27 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: GNav(
-          rippleColor: grey,
-          haptic: true,
-          tabBorderRadius: 15,
-          tabActiveBorder: Border.all(color: grey, width: 1),
-          tabBorder: Border.all(color: grey, width: 1),
-          tabShadow: [BoxShadow(color: grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
-          curve: Curves.easeOutExpo,
-          duration: 500.ms,
-          gap: 8,
-          color: grey, // unselected icon color
-          activeColor: pink, // selected icon and text color
-          iconSize: 24, // tab button icon size
-          tabBackgroundColor: pink.withOpacity(.1), // selected tab background color
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
-          tabs: [
-            GButton(
-              icon: LineIcons.home,
-              text: 'Home',
-            ),
-            GButton(
-              icon: LineIcons.heart_o,
-              text: 'Likes',
-            ),
-            GButton(
-              icon: LineIcons.search,
-              text: 'Search',
-            ),
-            GButton(
-              icon: LineIcons.user,
-              text: 'Profile',
-            )
-          ]),
+        rippleColor: grey,
+        haptic: true,
+        tabBorderRadius: 15,
+        tabActiveBorder: Border.all(color: grey, width: 1),
+        tabBorder: Border.all(color: grey, width: 1),
+        tabShadow: [BoxShadow(color: grey.withOpacity(0.5), blurRadius: 8)],
+        curve: Curves.easeOutExpo,
+        duration: 500.ms,
+        gap: 8,
+        color: grey,
+        activeColor: pink,
+        iconSize: 24,
+        tabBackgroundColor: pink.withOpacity(.1),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tabs: <GButton>[
+          GButton(icon: FontAwesome., text: 'Home'),
+          GButton(icon: LineIcons.heart_o, text: 'Likes'),
+          GButton(icon: LineIcons.search, text: 'Search'),
+          GButton(icon: LineIcons.user, text: 'Profile'),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: PageView.builder(
