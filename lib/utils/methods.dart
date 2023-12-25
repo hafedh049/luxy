3ed6b2d0b17b1fae4e1100eb2c4924c36c674067
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 16:26:33 +0100

    ModifiedM lib/utils/methods.dart M  pubspec.yaml

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,3 +1,4 @@
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hive/hive.dart';
 import 'package:luxy/utils/globals.dart';
 import 'package:path_provider/path_provider.dart';
@@ -15,4 +16,15 @@ Future<bool> load() async {
   } catch (e) {
     return false;
   }
+}
+
+void showToast(String message) {
+  Fluttertoast.showToast(
+    msg: message,
+    backgroundColor: pink.withOpacity(.6),
+    fontSize: 16,
+    gravity: ToastGravity.SNACKBAR,
+    toastLength: Toast.LENGTH_LONG,
+    textColor: white,
+  );
 }
