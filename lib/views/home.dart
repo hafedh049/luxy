27ffe7675139704a84421df9161826e27da7edd7
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 16:56:06 +0100

    ModifiedM lib/views/home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:luxy/utils/globals.dart';
 import 'package:luxy/views/profile.dart';
 
 class Home extends StatefulWidget {
@@ -25,8 +27,38 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: GNav(
-        tabs: <GButton>[],
-      ),
+          rippleColor: grey,
+          haptic: true,
+          tabBorderRadius: 15,
+          tabActiveBorder: Border.all(color: grey, width: 1), // tab button border
+          tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
+          tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
+          curve: Curves.easeOutExpo, // tab animation curves
+          duration: 500.ms, // tab animation duration
+          gap: 8, // the tab button gap between icon and text
+          color: Colors.grey[800], // unselected icon color
+          activeColor: Colors.purple, // selected icon and text color
+          iconSize: 24, // tab button icon size
+          tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
+          tabs: [
+            GButton(
+              icon: LineIcons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: LineIcons.heart_o,
+              text: 'Likes',
+            ),
+            GButton(
+              icon: LineIcons.search,
+              text: 'Search',
+            ),
+            GButton(
+              icon: LineIcons.user,
+              text: 'Profile',
+            )
+          ]),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: PageView.builder(
