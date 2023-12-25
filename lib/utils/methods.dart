9b3449c7d454f8318f81375b6214859da0bc80d4
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 16:22:33 +0100

    ModifiedM lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,6 +1,13 @@
 import 'package:hive/hive.dart';
+import 'package:luxy/utils/globals.dart';
+import 'package:path_provider/path_provider.dart';
 
 void load() async {
-  Hive.init(get);
-  user
+  Hive.init((await getApplicationDocumentsDirectory()).path);
+  user = await Hive.openBox("user");
+  if (user.isEmpty) {
+    await user.put("dark_mode", true);
+    await user.put("first_time", true);
+    await user.put("language", "en");
+  }
 }
