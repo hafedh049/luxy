0e6568ef8f5ed276de0e7498216c4e97a831ece0
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-25 16:47:03 +0100

    ModifiedM lib/views/home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:luxy/views/profile.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +9,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final PageController _pageController = PageController();
+  final List<Widget> _pages = <Widget>[
+    const Profile(),
+  ];
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: PageView.builder(
+        itemBuilder: (BuildContext context, int index) => _pages[index],
+      ),
+    );
   }
 }
