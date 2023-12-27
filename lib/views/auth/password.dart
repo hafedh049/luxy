23cd2656eb7d640790a06a299cc2d3e759576acc
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-27 17:23:14 +0100

    ModifiedM lib/views/auth/password.dart M  pubspec.yaml

--- lib/views/auth/password.dart
+++ lib/views/auth/password.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:luxy/utils/globals.dart';
 
 class Password extends StatefulWidget {
   const Password({super.key});
@@ -11,12 +13,49 @@ class _PasswordState extends State<Password> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Focus,
+      onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: dark,
+          leading: IconButton(
+            icon: const Icon(FontAwesome.chevron_left_solid, size: 15, color: white),
+            onPressed: () => Navigator.pop(context),
+          ),
+          title: const Text("Create new password", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+        ),
         body: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(
-            children: <Widget>[],
+            children: <Widget>[
+              Expanded(
+                child: SizedBox(),
+              ),
+              const Text("Address Details", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+              const SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: grey.withOpacity(.2)),
+                child: StatefulBuilder(
+                  builder: (BuildContext context, void Function(void Function()) _) {
+                    return TextField(
+                      controller: _passwordController,
+                      obscureText: _obscure,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(16),
+                        border: InputBorder.none,
+                        hintText: "Password",
+                        hintStyle: const TextStyle(color: grey),
+                        prefixIcon: const Icon(Bootstrap.lock_fill, size: 15, color: grey),
+                        suffixIcon: IconButton(
+                          onPressed: () => _(() => _obscure = !_obscure),
+                          icon: Icon(_obscure ? Bootstrap.eye_slash : Bootstrap.eye, size: 15, color: grey),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
           ),
         ),
       ),
