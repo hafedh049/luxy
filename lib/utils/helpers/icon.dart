bc6252aa22af0df266758b11bb204b883d73d326
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-27 11:29:49 +0100

    ModifiedM lib/views/auth/sign_in.dart ?? lib/utils/helpers/icon.dart

---
+++ lib/utils/helpers/icon.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:luxy/utils/globals.dart';
+
+class Iconed extends StatefulWidget {
+  const Iconed({super.key, required this.icon, required this.callback, this.iconSize = 15});
+  final IconData icon;
+  final void Function() callback;
+  final double iconSize;
+  @override
+  State<Iconed> createState() => _IconedState();
+}
+
+class _IconedState extends State<Iconed> {
+  bool _borderState = false;
+  bool _colorState = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onLongPressDown: (LongPressDownDetails details) async {
+        setState(() => _colorState = true);
+        await Future.delayed(300.ms);
+        setState(() => _borderState = true);
+      },
+      onLongPressUp: () async {
+        setState(() => _colorState = false);
+        await Future.delayed(300.ms);
+        setState(() => _borderState = false);
+        widget.callback();
+      },
+      onTapUp: (TapUpDetails details) async {
+        setState(() => _colorState = false);
+        await Future.delayed(500.ms);
+        setState(() => _borderState = false);
+        widget.callback();
+      },
+      onTapDown: (TapDownDetails details) async {
+        setState(() => _colorState = true);
+        await Future.delayed(300.ms);
+        setState(() => _borderState = true);
+      },
+      child: AnimatedContainer(
+        duration: 500.ms,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(border: Border.all(width: 1, color: _borderState ? pink : transparent), shape: BoxShape.circle, color: _colorState ? pink : transparent),
+        child: Icon(widget.icon, size: widget.iconSize, color: _colorState ? dark : white),
+      ),
+    );
+  }
+}
--- lib/views/auth/sign_in.dart
+++ lib/views/auth/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:luxy/utils/globals.dart';
+import 'package:luxy/utils/helpers/icon.dart';
 import 'package:luxy/utils/helpers/layvy.dart';
 
 class SignIn extends StatefulWidget {
@@ -26,65 +27,97 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          const Layvy(),
-          const Text("Login to your account", style: TextStyle(color: white, fontSize: 20, fontWeight: FontWeight.w500)),
-          TextField(
-            controller: _emailController,
-            decoration: const InputDecoration(
-              contentPadding: EdgeInsets.all(16),
-              border: InputBorder.none,
-              hintText: "E-mail",
-              prefixIcon: Icon(Bootstrap.envelope, size: 15, color: grey),
-            ),
-          ),
-          TextField(
-            controller: _emailController,
-            obscureText: !_obscure,
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.all(16),
-              border: InputBorder.none,
-              hintText: "Password",
-              prefixIcon: const Icon(Bootstrap.lock_fill, size: 15, color: grey),
-              suffixIcon: StatefulBuilder(
-                builder: (BuildContext context, void Function(void Function()) _) {
-                  return IconButton(
-                    onPressed: () => _(() => _obscure = !_obscure),
-                    icon: Icon(_obscure ? Bootstrap.eye_slash : Bootstrap.eye, size: 15, color: grey),
-                  );
-                },
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            const Layvy(),
+            const Text("Login to your account", style: TextStyle(color: white, fontSize: 20, fontWeight: FontWeight.w500)),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(16),
+                border: InputBorder.none,
+                hintText: "E-mail",
+                prefixIcon: Icon(Bootstrap.envelope, size: 15, color: grey),
               ),
             ),
-          ),
-          Row(
-            children: <Widget>[
-              StatefulBuilder(
-                builder: (BuildContext context, void Function(void Function()) _) {
-                  return Checkbox(
-                    value: _rememberMe,
-                    checkColor: white,
-                    activeColor: pink,
-                    onChanged: (bool? value) => _(() => _rememberMe = !_rememberMe),
-                  );
-                },
+            TextField(
+              controller: _emailController,
+              obscureText: !_obscure,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.all(16),
+                border: InputBorder.none,
+                hintText: "Password",
+                prefixIcon: const Icon(Bootstrap.lock_fill, size: 15, color: grey),
+                suffixIcon: StatefulBuilder(
+                  builder: (BuildContext context, void Function(void Function()) _) {
+                    return IconButton(
+                      onPressed: () => _(() => _obscure = !_obscure),
+                      icon: Icon(_obscure ? Bootstrap.eye_slash : Bootstrap.eye, size: 15, color: grey),
+                    );
+                  },
+                ),
               ),
-              const SizedBox(width: 10),
-              const Text("Remember Me", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            decoration: BoxDecoration(color: pink, borderRadius: BorderRadius.circular(15)),
-            alignment: Alignment.center,
-            child: const Text("Sign In", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: const Text("Forgot the password?", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
-          ),
-        ],
+            ),
+            Row(
+              children: <Widget>[
+                StatefulBuilder(
+                  builder: (BuildContext context, void Function(void Function()) _) {
+                    return Checkbox(
+                      value: _rememberMe,
+                      checkColor: white,
+                      activeColor: pink,
+                      onChanged: (bool? value) => _(() => _rememberMe = !_rememberMe),
+                    );
+                  },
+                ),
+                const SizedBox(width: 10),
+                const Text("Remember Me", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+              ],
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                decoration: BoxDecoration(color: pink, borderRadius: BorderRadius.circular(15)),
+                alignment: Alignment.center,
+                child: const Text("Sign In", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: const Text("Forgot the password?", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+            ),
+            const Row(
+              children: <Widget>[
+                Divider(thickness: .5, height: .5, color: grey, indent: 25, endIndent: 25),
+                Text("OR CONTINUE WITH", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+                Divider(thickness: .5, height: .5, color: grey, indent: 25, endIndent: 25),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Iconed(icon: Bootstrap.facebook, callback: () {}),
+                Iconed(icon: Bootstrap.google, callback: () {}),
+                Iconed(icon: Bootstrap.apple, callback: () {}),
+              ],
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Iconed(icon: Bootstrap.facebook, callback: () {}),
+                  Iconed(icon: Bootstrap.google, callback: () {}),
+                  Iconed(icon: Bootstrap.apple, callback: () {}),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
