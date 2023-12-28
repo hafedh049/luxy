bbce8dad579d991dddf24d504097734b61c3262f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-28 11:13:18 +0100

    ModifiedM lib/views/my_orders.dart

--- lib/views/my_orders.dart
+++ lib/views/my_orders.dart
@@ -16,10 +16,30 @@ class _MyOrdersState extends State<MyOrders> {
       appBar: AppBar(
         backgroundColor: dark,
         leading: IconButton(icon: const Icon(FontAwesome.chevron_left_solid, size: 15, color: white), onPressed: () => Navigator.pop(context)),
-        title: const Text("Shipping Address", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+        actions: <Widget>[
+          IconButton(icon: const Icon(FontAwesome.searchengin_brand, size: 15, color: white), onPressed: () {}),
+        ],
+        title: const Text("My Orders", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TabBarView(
+          children: <Tab>[
+            Tab(
+              text: "Active",
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) => Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: grey.withOpacity(.3)),
+                  child: Column(
+                    children: <Widget>[],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
