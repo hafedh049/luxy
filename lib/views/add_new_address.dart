f63fbb88093ebb3fb715b1cda7d91c37b1a5153a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-31 09:55:33 +0100

    ModifiedM lib/views/add_new_address.dart

--- lib/views/add_new_address.dart
+++ lib/views/add_new_address.dart
@@ -13,13 +13,35 @@ class AddNewAddress extends StatefulWidget {
 class _AddNewAddressState extends State<AddNewAddress> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
         backgroundColor: dark,
         leading: IconButton(icon: const Icon(FontAwesome.chevron_left_solid, size: 15, color: white), onPressed: () => Navigator.pop(context)),
         actions: <Widget>[
           IconButton(icon: const Icon(FontAwesome.ellipsis_solid, size: 15, color: white), onPressed: () {}),
         ],
-        title: const Text("Order Details", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
-      ),body: ,);
+        title: const Text("Add New Address", style: TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w500)),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Divider(thickness: .5, height: .5, color: grey),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Address Details", style: TextStyle(color: white, fontSize: 18, fontWeight: FontWeight.w500)),
+                  const SizedBox(height: 10),
+                  const Divider(thickness: .5, height: .5, color: grey),
+                  const SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
