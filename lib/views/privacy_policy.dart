64ef9ad8867e0728253e06c9013320511f9b2b73
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-31 15:13:45 +0100

    ModifiedM lib/views/privacy_policy.dart

--- lib/views/privacy_policy.dart
+++ lib/views/privacy_policy.dart
@@ -127,27 +127,26 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
                                             mainAxisSize: MainAxisSize.min,
                                             children: <Widget>[
                                               if (item['subtitle'] != null) Flexible(child: Container(margin: const EdgeInsets.only(bottom: 10), child: Text("${data[index]['subtitles'].indexOf(item) + 1}. ${item['subtitle']}", style: const TextStyle(color: pink, fontSize: 18, fontWeight: FontWeight.bold)))),
-                                              for (final Map<String, dynamic> element in item["data"])
+                                              for (final dynamic element in item["data"])
                                                 Column(
                                                   crossAxisAlignment: CrossAxisAlignment.start,
                                                   mainAxisSize: MainAxisSize.min,
                                                   children: <Widget>[
-                                                    for (final dynamic uplet in element["data"])
-                                                      uplet is String
-                                                          ? const SizedBox()
-                                                          : Container(
-                                                              margin: const EdgeInsets.only(bottom: 10),
-                                                              child: RichText(
-                                                                text: TextSpan(
-                                                                  children: <TextSpan>[
-                                                                    const TextSpan(text: "●", style: TextStyle(color: pink, fontSize: 16, fontWeight: FontWeight.bold)),
-                                                                    TextSpan(text: " $uplet", style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.bold)),
-                                                                  ],
-                                                                ),
-                                                              ),
-                                                            )
+                                                    Flexible(child: Container(margin: const EdgeInsets.only(bottom: 10), child: Text(element["data"][0], style: const TextStyle(color: white, fontSize: 18, fontWeight: FontWeight.bold)))),
+                                                    for (final String uplet in element["data"].skip(1))
+                                                      Container(
+                                                        margin: const EdgeInsets.only(bottom: 10),
+                                                        child: RichText(
+                                                          text: TextSpan(
+                                                            children: <TextSpan>[
+                                                              const TextSpan(text: "●", style: TextStyle(color: pink, fontSize: 16, fontWeight: FontWeight.bold)),
+                                                              TextSpan(text: " $uplet", style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.bold)),
+                                                            ],
+                                                          ),
+                                                        ),
+                                                      ),
                                                   ],
-                                                ),
+                                                )
                                             ],
                                           ),
                         ],
