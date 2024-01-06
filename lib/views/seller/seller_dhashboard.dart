e2cdafc149f43367fbc7a9dbc460c5fbeebc8682
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-06 12:14:17 +0100

    ModifiedM lib/views/seller/seller_dhashboard.dart

--- lib/views/seller/seller_dhashboard.dart
+++ lib/views/seller/seller_dhashboard.dart
@@ -125,8 +125,36 @@ class _SellerDashboardState extends State<SellerDashboard> {
                             const Spacer(),
                             SizedBox(
                               height: 40,
+                              width: 200,
                               child: LineChart(
-                                LineChartData(),
+                                LineChartData(
+                                  gridData: const FlGridData(show: false),
+                                  titlesData: const FlTitlesData(show: false),
+                                  borderData: FlBorderData(show: false),
+                                  minX: 0,
+                                  maxX: 11,
+                                  minY: 0,
+                                  maxY: 6,
+                                  lineBarsData: <LineChartBarData>[
+                                    LineChartBarData(
+                                      spots: const <FlSpot>[
+                                        FlSpot(0, 3),
+                                        FlSpot(2.6, 2),
+                                        FlSpot(4.9, 5),
+                                        FlSpot(6.8, 3.1),
+                                        FlSpot(8, 4),
+                                        FlSpot(9.5, 3),
+                                        FlSpot(11, 4),
+                                      ],
+                                      isCurved: true,
+                                      gradient: LinearGradient(colors: <Color>[item["chart_color"].withOpacity(.6), white.withOpacity(.4)]),
+                                      barWidth: 5,
+                                      isStrokeCapRound: true,
+                                      dotData: const FlDotData(show: true),
+                                      belowBarData: BarAreaData(show: true, gradient: LinearGradient(colors: <Color>[item["chart_color"].withOpacity(.6), white.withOpacity(.4)])),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ],
